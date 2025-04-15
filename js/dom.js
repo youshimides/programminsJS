c9bf34b272ce79d922fa56dec21c2d9fb6fbf2ae
youshimides <khamitovgleb@mail.ru>
2025-04-15 12:54:03 +0500

    dom

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+/*1. выбор элементов на странице */
+/*1.1. выбор по id */
+const h1 = document.getElementById("title");
+console.log(h1);
+/*1.2 выбор одного элемента по селектору  */
+const menu = document.querySelector(".menu li:last-child");
+console.log(menu);
